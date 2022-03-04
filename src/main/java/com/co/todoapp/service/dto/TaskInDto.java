2554a4a2a8726a0090ba0f58a2f6e826f1936611
Camilo Castro <camilo.castrotech@gmail.com>
2022-03-04 00:00:29 -0500

    Create Task dto

---
+++ src/main/java/com/co/todoapp/service/dto/TaskInDto.java
@@ -0,0 +1,12 @@
+package com.co.todoapp.service.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class TaskInDto {
+    private String title;
+    private String description;
+    private LocalDateTime eta;
+}
